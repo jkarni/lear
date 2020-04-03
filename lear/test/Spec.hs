cb3dadb8ca997a17d15ec74823cf3b816b691df5
Julian K. Arni <jkarni@gmail.com>
2020-04-03 21:12:43 +0200

    Fix tests

--- lear/test/Spec.hs
+++ lear/test/Spec.hs
@@ -34,7 +34,7 @@ learnOneSpec =
     it "returns the input if correct" $ hedgehog $ do
       x <- H.forAll $ H.filter (/= 0) $ H.float (HR.constant (-100) 100)
       y <- H.forAll $ H.float (HR.constant (-100) 100)
-      learnOne linear x y (x * y) === (x, y)
+      learnOne linear x y (x * y) ~=~ (x, y)
     it "returns the two fixes if incorrect" $ hedgehog $ do
       x <- H.forAll $ H.filter (/= 0) $ H.float (HR.constant (-100) 100)
       y <- H.forAll $ H.float (HR.constant (-100) 100)
@@ -63,17 +63,7 @@ atRateSpec =
 --
 -- This is solvable from one datapoint, which makes testing easier.
 linear :: Lear Float Float Float
--- linear = backpropToLear $ \p x -> p * x
 linear = param * input
-
--- Change to this:
-
-{-
-data P = P {weight :: Float, bias :: Float}
-
-linear' :: Lear P Float Float
-linear' = (param . look @"weight") * input + (param . look @"bias")
--}
 
 -- ** Approximate Equality
 
@@ -93,7 +83,6 @@ instance ApproximateEq Float
 
 instance (ApproximateEq a, ApproximateEq b) => ApproximateEq (a, b) where
   (a0, a1) ~~ (b0, b1) = a0 ~~ b0 && a1 ~~ b1
-
 {-
 adjunctSpec :: Spec
 adjunctSpec = describe "adjunct" $ do
@@ -111,25 +100,3 @@ cataLSpec = describe "cataL"
     forwards (cataL l) 0 [2, 3, 1] `shouldBe` 0
     forwards (cataL l) 1 [2, 3, 1] `shouldBe` 6
 -}
-
--- Annoyingly, can't import lear-backprop (see
--- https://github.com/commercialhaskell/stack/issues/4786). So we just copy it
--- backpropToLear ::
---   (Backprop p, Backprop a, Backprop b, Fractional p, Fractional a, Num b) =>
---   (forall s. (Reifies s W) => BVar s p -> BVar s a -> BVar s b) ->
---   Lear p a b
-backpropToLear ::
-  (Backprop p, Floating (Scalar p), Backprop a, Fractional p, Fractional a, VectorSpace a, VectorSpace p, InnerSpace b, Num b, Scalar b ~ Scalar p, Scalar a ~ Scalar p) =>
-  (forall s. (Reifies s W) => BVar s p -> BVar s a -> BVar s b) ->
-  Lear p a b
-backpropToLear f = Lear f'
-  where
-    f' p a =
-      let (b, g) = backpropWith2 f p a
-       in ( b,
-            \b' ->
-              let (p', a') = g 1
-               in ( const (p - (magnitude (b - b') *^ recip p')),
-                    a - (magnitude (b - b') *^ recip a')
-                  )
-          )
